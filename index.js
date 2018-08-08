202e73e270d0d80b2c715672d21214a1376c00a5
Annie Wang <anwang911@gmail.com>
2018-08-08 14:20:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your code in this file!
-// const currentUser = 'Annie Wang';
+const currentUser = 'Annie Wang';
 
-// const welcomeMessage = "Welcome to Klossybook, ${currentUser}!";
-const currentUser = 'Grace Hopper';
+const welcomeMessage = "Welcome to Klossybook, ${currentUser}!";
+// const currentUser = 'Grace Hopper';
  
-const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
+// const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
 
 // const excitedWelcomeMessage = ""
